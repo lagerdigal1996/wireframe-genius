e093e6f01b98dc03c92088c5b460352b599f0008
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-10-01 10:14:07 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const app = document.getElementById('app');
+    app.innerHTML = '<p>This is where the wireframe creation interface will be.</p>';
+});
